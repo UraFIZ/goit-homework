cfe2158825a8c33fe1a13db671ef741432a6094c
Yurii Kratser <yuriikratser@IKRATSER-MAC.local>
2024-05-05 11:18:12 +0200

    correct task 1

--- goit-algo-hw_3/task_1/get_days_from_today.py
+++ goit-algo-hw_3/task_1/get_days_from_today.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 
-
 def get_days_from_today(date: str) -> int:
-    input_date = datetime.strptime(date, '%Y-%m-%d')
-    current_date = datetime.now()
-    diff = current_date - input_date
-    
-    return diff.days
+    try:
+        input_date = datetime.strptime(date, '%Y-%m-%d')
+        current_date = datetime.now()
+        diff = current_date - input_date
+        return diff.days
+    except ValueError as e:
+        print(f"Виникла помилка з датою: {e}")
+        return None  # Повертаємо None, якщо дата невірна
 
-print(get_days_from_today('2024-04-30'))
-
-
+# Приклад використання
+result = get_days_from_today('2024-04-30')
+if result is not None:
+    print(f"Кількість днів від вказаної дати: {result}")
+else:
+    print("Будь ласка, перевірте формат введеної дати, наприклад, 2024-12-31.")
