f5258b864f9a0f5ff5b0e33e89b747bf975aab75
urafiz <kratser88@gmail.com>
2024-05-14 16:42:34 +0200

    tiny correction

--- goit-pycore-hw-06/utils.py
+++ goit-pycore-hw-06/utils.py
@@ -20,7 +20,7 @@ def format_table(headers, rows):
 
 
 def parse_command():
-    parser = argparse.ArgumentParser(prog='', description='Manage your address book. Available commands: add, edit, delete, find, list, close, exist. Have any questions? Type "-h, --help". Have fun!')
+    parser = argparse.ArgumentParser(prog='', description='Manage your address book. Have any questions? Type "-h, --help". Have fun!')
     subparsers = parser.add_subparsers(dest='command')
 
     add_parser = subparsers.add_parser('add', help='add [name] [phone1] - Add a new record. Example: "add John 1234567890"')
@@ -40,6 +40,6 @@ def parse_command():
 
     subparsers.add_parser('list', help='List of all records. Example: "list"')
     subparsers.add_parser('close', help='Close the app and save the data. Example: "close"')
-    subparsers.add_parser('exist', help='Close the app and save the data. Example: "exist"')
+    subparsers.add_parser('exit', help='Close the app and save the data. Example: "exist"')
 
     return parser
